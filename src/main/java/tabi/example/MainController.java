282e7d5562827a8a504aa4e05c14a60d83333178
Eto Koshi <st022474@m03.kyoto-kcg.ac.jp>
2025-11-20 16:26:43 +0900

    Fist commit

---
+++ src/main/java/tabi/example/MainController.java
@@ -0,0 +1,25 @@
+package tabi.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+
+public class MainController {
+    @FXML
+    private TextField TextFieldA;
+
+    @FXML
+    private TextField TextFieldB;
+
+    @FXML
+    private Label Answer;
+
+    @FXML
+    private TextArea memo;
+
+    @FXML
+    private void initialize() {
+
+    }
+}
